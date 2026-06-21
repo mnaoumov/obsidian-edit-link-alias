3155748defabf23effe1a242386b5f977003614f
Michael Naumov <mnaoumov@gmail.com>
2026-06-20 22:49:39 -0600

    refactor: update template

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,8 +1,3 @@
-import type {
-  App,
-  PluginManifest
-} from 'obsidian';
-
 import { AppActiveFileProvider } from 'obsidian-dev-utils/obsidian/active-file-provider';
 import { CommandHandlerComponent } from 'obsidian-dev-utils/obsidian/command-handlers/command-handler-component';
 import { PluginCommandRegistrar } from 'obsidian-dev-utils/obsidian/command-registrar';
@@ -12,17 +7,15 @@ import { PluginBase } from 'obsidian-dev-utils/obsidian/plugin/plugin';
 import { EditCommandHandler } from './edit-command-handler.ts';
 
 export class Plugin extends PluginBase {
-  public constructor(app: App, manifest: PluginManifest) {
-    super(app, manifest);
-
-    const menuEventRegistrar = this.addChild(new MenuEventRegistrarComponent(app));
+  protected override onloadImpl(): void {
+    const menuEventRegistrar = this.addChild(new MenuEventRegistrarComponent(this.app));
     this.addChild(
       new CommandHandlerComponent({
-        activeFileProvider: new AppActiveFileProvider(app),
-        commandHandlers: [new EditCommandHandler(app)],
+        activeFileProvider: new AppActiveFileProvider(this.app),
+        commandHandlers: [new EditCommandHandler(this.app)],
         commandRegistrar: new PluginCommandRegistrar(this),
         menuEventRegistrar,
-        pluginName: manifest.name
+        pluginName: this.manifest.name
       })
     );
   }
